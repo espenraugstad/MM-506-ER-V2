dbd4884dc3d6b79c375478acd0999dfa70ae662f
espenraugstad <espen.raugstad@gmail.com>
2023-02-20 16:49:45 +0100

    Added username and sillytoken to login

--- public/scripts/presenter-dashboard.js
+++ public/scripts/presenter-dashboard.js
@@ -1,12 +1,24 @@
 /*** HTML ELEMENTS ***/
+const username = document.getElementById('username');
 const newPresentation = document.getElementById('new');
 const logout = document.getElementById('logout');
+
+/*** GLOBAL VARIABLES ***/
+let presentations = [];
 
 /*** EVENT LISTENERS ***/
 newPresentation.addEventListener("click", ()=>{
     location.href = "authoring.html";
-})
+});
 
 logout.addEventListener("click", ()=>{
     location.href = "index.html";
-})
+});
+
+/*** FUNCTIONS ***/
+(function() {
+    username.innerHTML = localStorage.getItem("user_name");
+
+    // Retrieve presentations
+
+})();
