8956597bb37b1c292c286a1fae417e1df94659e1
espenraugstad <espen.raugstad@gmail.com>
2023-02-21 11:39:21 +0100

    Can parse presentations with correct global options

--- public/scripts/authoring.js
+++ public/scripts/authoring.js
@@ -3,14 +3,20 @@ import Config from "./modules/server-com.js";
 /*** HTML ELEMENTS ***/
 const presentationTitle = document.getElementById('presentation-title');
 const editor = document.getElementById('editor');
+const slidesPreview = document.getElementById('slides-preview');
 const save = document.getElementById('save');
 const dash = document.getElementById('dash');
 const start = document.getElementById('start');
 
 /*** GLOBAL VARIABLES ***/
 let currentPresentation = null;
+let slides = [];
 
 /*** EVENT LISTENERS ***/
+save.addEventListener("click", ()=>{
+    parsePresentation();
+})
+
 dash.addEventListener("click", ()=>{
     console.log("Check for save before returning to dashboard");
     //location.href = "presenter-dashboard.html";
@@ -44,4 +50,47 @@ async function getPresentation(){
 
 function previewPresentation(){
     console.log("Previewing");
+    //parsePresentation();
+}
+
+function parsePresentation(){
+    let raw = marked.parse(editor.value);
+
+    // Split by hr's
+    let rawInputs = raw.split("<hr>");
+
+    // First input will be global options if it contains comments
+    let globalOptions = {
+        theme: ""
+    };
+    if(rawInputs[0].includes("<!--")){
+        // Input contains global options for this presentation
+        // Split on newline to get all properties
+        let rawProperties = rawInputs[0].split("\n");
+        // Filter out all relevant properties
+        for(let rawEl of rawProperties){
+            if(rawEl.split(":").length > 1){
+                // The first element of the split is a property key, second is the property value
+                let possibleKey = rawEl.split(":")[0].toLowerCase().trim();
+                let possibleValue = rawEl.split(":")[1].toLowerCase().trim();
+
+                // Check if the possible key is in the keys of the global options object
+                if(Object.keys(globalOptions).some(el => el === possibleKey)){
+                    globalOptions[possibleKey] = possibleValue;
+                } else {
+                    console.log("Invalid key");
+                }
+            }
+        }
+
+        slides = rawInputs.slice(1); 
+
+
+    } else {
+        slides = rawInputs;
+    }
+
+    slidesPreview.innerHTML = slides.join("");
+    console.log(slides);
+    console.log(globalOptions);
 }
